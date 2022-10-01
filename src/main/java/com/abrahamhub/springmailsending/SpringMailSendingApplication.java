69fddb09dd9be399b4c4810e202dd2fafdcb8800
AbrahamHub <apple_abraham@hotmail.com>
2022-10-01 17:14:41 -0500

    1.0

---
+++ src/main/java/com/abrahamhub/springmailsending/SpringMailSendingApplication.java
@@ -0,0 +1,30 @@
+package com.abrahamhub.springmailsending;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.*;
+
+@SpringBootApplication
+@RestController
+public class SpringMailSendingApplication {
+    @Autowired
+    private EmailSendService senderService;
+    public static void main(String[] args) {
+        SpringApplication.run(SpringMailSendingApplication.class, args);
+    }
+    @PostMapping (value = "/email")
+    public void getEmail(@RequestBody requestData data) {
+        sendMail(data.getEmail(), data.getName());
+    }
+    public void sendMail (String EmailV, String NameV ) {
+        senderService.sendEmail(EmailV,"Hola "+NameV+" trabajemos juntos!!!👨🏻‍💻","Me presento soy Abraham Castañeda Quintero, un miembro proactivo de la humanidad y entusiasta de la tecnología con una vocación de servicio, además nunca parar de aprender.\uD83D\uDE80\uD83E\uDDE0\n" +
+                "\n" +
+                "Tengo una buena trayectoria en la rama de la informática y del desarrollo de software, desde los 15 años descubrí mi gusto y vocación por la misma.\n" +
+                "\n" +
+                "¿Quieres ver mi trabajo en GitHub? https://github.com/AbrahamHub \uD83D\uDE40\n" +
+                "Vuelve a mi portafolio y descarga mi CV https://abrahamc.dev/ \uD83D\uDCC3\n" +
+                "\n" +
+                "Puedes responder a este correo y en poco tiempo contactaré contigo \uD83E\uDEE1");
+    }
+}
---
+++ src/main/java/com/abrahamhub/springmailsending/requestData.java
@@ -0,0 +1,9 @@
+package com.abrahamhub.springmailsending;
+public class requestData {
+    public String name;
+    public String email;
+    public String getEmail() {
+        return email;
+    }
+    public String getName() { return name; }
+}
