f395408d23e5df5c6c20dfc8fd56d371395cdc9e
AbrahamHub <apple_abraham@hotmail.com>
2023-03-17 16:15:05 -0600

    #4

--- src/main/java/com/abrahamhub/springmailsending/SpringMailSendingApplication.java
+++ src/main/java/com/abrahamhub/springmailsending/SpringMailSendingApplication.java
@@ -30,7 +30,7 @@ public class SpringMailSendingApplication {
                 "Tengo una buena trayectoria en la rama de la informática y del desarrollo de software, desde los 15 años descubrí mi gusto y vocación por la misma.\n" +
                 "\n" +
                 "¿Quieres ver mi trabajo en GitHub? https://github.com/AbrahamHub \uD83D\uDE40\n" +
-                "Vuelve a mi portafolio y descarga mi CV https://abrahamc.dev/ \uD83D\uDCC3\n" +
+                "Vuelve a mi portafolio y descarga mi CV https://abraham.contact/ \uD83D\uDCC3\n" +
                 "\n" +
                 "Puedes responder a este correo y en poco tiempo contactaré contigo \uD83E\uDEE1");
     }
